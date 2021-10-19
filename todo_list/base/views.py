72b86bb47788615e11909266b168acc91b2d9440
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-10-19 10:48:42 +0200

    Task creat and delte changes

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -26,7 +26,8 @@ class TaskList(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['color'] = 'red'
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
         return context
 
 
@@ -38,13 +39,17 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class taskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(taskCreate, self).form_valid(form)
 
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 
